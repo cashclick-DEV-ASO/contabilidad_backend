618fe287430ca1167751f0e075e2ed8dbc412b2c
Alberto Soto <albertosoto@nube.unadmexico.mx>
2023-10-23 16:41:19 -0600

    Config GIT

--- middlewares/sesion.js
+++ middlewares/sesion.js
@@ -9,6 +9,7 @@ export const validaSesion = modelos => {
     const listaBlanca = ["/login", "/status"]
     const controlador = new SesionContoller(modelos.sesion)
 
+    // Regresa una función que valida la sesión
     return async (req, res, next) => {
         if (!listaBlanca.includes(req.path)) await controlador.validaSesion(req, res)
         if (res.statusCode >= 200 && res.statusCode < 300) next()
