3660c9001469e3913c71b0d84bb5fe15bfd82286
Alberto Soto <albertosoto@nube.unadmexico.mx>
2023-10-27 20:13:38 -0600

    cors error

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,7 +7,7 @@ export const configuracionCORS = (aceptados = process.env.ORIGINS || "*") => cor
 
 		if (!origin)
 			return callback(null, true)
-
+		console.log(origin);
 		return callback("Not allowed")
 	}
 })
